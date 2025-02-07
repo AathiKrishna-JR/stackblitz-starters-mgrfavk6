6f2925e0c28cf54af9b748bb174092039cc85a06
AathiKrishna-JR <aathi3612@gmail.com>
2025-02-07 12:21:51 +0530

    1

--- src/app/contact-list/contact.service.ts
+++ src/app/contact-list/contact.service.ts
@@ -1,25 +1,29 @@
-import { Injectable, Signal, WritableSignal, signal } from '@angular/core';
+import { Injectable, WritableSignal, signal } from '@angular/core';
+
+export interface Contact {
+  name: string;
+  phone: string;
+  email: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactService {
-  private contacts: WritableSignal<{ name: string; phone: string; email: string }[]> = signal([
+  private contacts: WritableSignal<Contact[]> = signal([
     { name: 'John Doe', phone: '123-456-7890', email: 'john@example.com' },
     { name: 'Jane Smith', phone: '987-654-3210', email: 'jane@example.com' }
   ]);
 
-  getContacts(): Signal<{ name: string; phone: string; email: string }[]> {
-    return this.contacts.asReadonly(); // Return a read-only signal
+  getContacts() {
+    return this.contacts;
   }
 
-  addContact(contact: { name: string; phone: string; email: string }) {
-    this.contacts.update((contacts: { name: string; phone: string; email: string }[]) => [...contacts, contact]);
+  addContact(contact: Contact) {
+    this.contacts.update((contacts) => [...contacts, contact]);
   }
 
   deleteContact(index: number) {
-    this.contacts.update((contacts: { name: string; phone: string; email: string }[]) => 
-      contacts.filter((_, i) => i !== index)
-    );
+    this.contacts.update((contacts) => contacts.filter((_, i) => i !== index));
   }
 }
--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { ContactListComponent } from '../contact-list/contact-list.component';
+import { AddContactComponent } from '../add-contact/add-contact.component';
 
 @Component({
   selector: 'app-contacts',
-  imports: [ContactListComponent],
+  imports: [ContactListComponent,AddContactComponent],
   templateUrl: './contacts.component.html',
   styleUrl: './contacts.component.css'
 })
