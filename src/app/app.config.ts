4385d61ef9a2aa0ce9e97d0cc122194110b72bc6
AathiKrishna-JR <aathi3612@gmail.com>
2025-02-07 15:24:08 +0530

    1

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,9 +2,11 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { ContactsComponent } from './contacts/contacts.component';
+import { AddContactComponent } from './add-contact/add-contact.component';
 
 const routes: Routes = [
-  { path: '', component: ContactsComponent}
+  { path: '', component: ContactsComponent},
+  { path: './addContacts', component: AddContactComponent}
 ];
 
 export const appConfig: ApplicationConfig = {
--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ContactListComponent } from '../contact-list/contact-list.component';
 import { AddContactComponent } from '../add-contact/add-contact.component';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-contacts',
@@ -9,5 +10,13 @@ import { AddContactComponent } from '../add-contact/add-contact.component';
   styleUrl: './contacts.component.css'
 })
 export class ContactsComponent {
-
+  private router = inject(Router);
+  private activatedRoute = inject(ActivatedRoute);
+  onAddTask(){
+    this.router.navigate(['./addContacts'],{
+      relativeTo: this.activatedRoute,
+      onSameUrlNavigation:'reload',
+      queryParamsHandling : 'preserve',
+    })
+  }
 }
