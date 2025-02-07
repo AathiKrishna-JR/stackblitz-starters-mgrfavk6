971168008b9b092b099417740c3dd03b1dccf1fb
AathiKrishna-JR <aathi3612@gmail.com>
2025-02-07 11:52:01 +0530

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
+  imports : [HeaderComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
---
+++ src/app/app.config.ts
@@ -0,0 +1,13 @@
+import { ApplicationConfig } from '@angular/core';
+import { provideRouter, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ContactsComponent } from './contacts/contacts.component';
+
+const routes: Routes = [
+  { path: '', component: AppComponent },
+  { path: 'contacts', component: ContactsComponent}
+];
+
+export const appConfig: ApplicationConfig = {
+  providers: [provideRouter(routes)]
+};
